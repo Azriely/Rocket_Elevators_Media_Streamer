0af12f690e274e337924e522ce8877642749061d
Azriely <dandurand14m@gmail.com>
2021-12-03 12:32:45 -0500

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
 get "home/login"
 get "/" => "home#index", :as => "root"
 post "/" , to: "home#create"
-post "/intervention" , to: "intervention#create"
 get "residential" => "home#residential"
 get "commercial" => "home#commercial"
 get "quote" => "home#quote"
 get "/intervention" => "home#intervention"
 post "getData" => "quote#getData"
+post "/intervention" , to: "intervention#create"
 get 'building_select/:customerID', to: 'intervention#building_select'
 get 'battery_select/:buildingID', to: 'intervention#battery_select'
 get 'column_select/:batteryID', to: 'intervention#column_select'
